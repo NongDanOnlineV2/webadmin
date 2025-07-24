c107c9c2c11acb1f75259698a9d39833e59db8a8
Tie902 <tiendmps40555@gmail.com>
2025-07-24 17:07:40 +0700

    noooo

--- src/pages/dashboard/user/users.jsx
+++ src/pages/dashboard/user/users.jsx
@@ -219,14 +219,6 @@ export default function Users() {
         });
       }
 
-      if (selectedUser.addresses?.[0]?.id) {
-        await axios.put(`${BaseUrl}/user-addresses/${selectedUser.addresses[0].id}`, {
-          address: formData.addresses[0]
-        }, {
-          headers: { Authorization: `Bearer ${token}` }
-        });
-      }
-
       alert("Cập nhật thành công!");
       fetchUsers();
       setEditOpen(false);
