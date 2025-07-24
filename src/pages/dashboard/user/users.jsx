ee7560445917bcdb815b2eb81a212ce18052e561
Tie902 <tiendmps40555@gmail.com>
2025-07-24 16:15:38 +0700

    Revert "Merge branch 'tien' into dev"
    This reverts commit 34792270d4907c12fe69eab7ca21cee92f2b6fa4, reversing
    changes made to cc968fb39f66157b66f912ede5029318b56a96bd.

--- src/pages/dashboard/user/users.jsx
+++ src/pages/dashboard/user/users.jsx
@@ -78,7 +78,6 @@ export default function Users() {
 
       const res = await api.get(`${BaseUrl}/admin-users`, { params });
       const usersData = res.data?.data || [];
-
       const postMap = {};
       allPosts.current.forEach(p => {
         const uid = p.userId || p.authorId?.id;
