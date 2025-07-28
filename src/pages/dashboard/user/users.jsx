a0bd08af1910cd8a393ce6273bc2f936dee36055
thanhnt0208 <thanhnguyen28216@gmail.com>
2025-07-28 13:26:14 +0700

    Update users.jsx

--- src/pages/dashboard/user/users.jsx
+++ src/pages/dashboard/user/users.jsx
@@ -41,7 +41,7 @@ export default function Users() {
 
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const BaseUrl = "https://api-ndolv2.nongdanonline.cc";
+
 const fetchAllData = async () => {
     try {
       const getAllPages = async (endpoint) => {
